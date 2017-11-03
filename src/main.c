5645e4ca5d19ee9f1052c5292888ce414ff8e138
paoli4u <cedric.paoli@telecomnancy.eu>
2017-11-03 20:29:32 +0100

    ReadMe creation + Patch in the option

--- src/main.c
+++ src/main.c
@@ -29,12 +29,7 @@ int main(int argc, char const *argv[]) {
       if (!strcmp(argv[1],"-c")) {
         ns = creationAuto(ns,argv[2]);
         validMain(ns);
-      } else if (!strcmp(argv[1],"-cl")) {
-        fprintf(stderr, "ARGV[0] : %s, ARGV[1] : %s, ARGV[2] : %s, ARGV[3] : %s,\n", argv[0],argv[1],argv[2],argv[3]);
-        ns = creationManual(ns);
-        learning(ns,argv[2]);
-        validMain(ns);
-      } else{
+      }  else{
         fprintf(stderr, "error : unknown switch %s\n",argv[1]);
         usage();
       }
@@ -44,10 +39,6 @@ int main(int argc, char const *argv[]) {
       if (!strcmp(argv[1],"-l")) {
         ns = loadNetwork(argv[2]);
         learningRetroPropagation(ns,argv[3]);
-        validMain(ns);
-      } else if (strcmp(argv[1],"-cl")) {
-        ns = creationAuto(ns,argv[2]);
-        learning(ns,argv[3]);
         validMain(ns);
       } else if(strcmp(argv[1],"-w")){
         ns = loadNetwork(argv[2]);
@@ -73,8 +64,6 @@ void usage(){
   fprintf(stderr, "     -c                            creation of a network\n");
   fprintf(stderr, "     -c  <file.ai>                 creation of a default network from data in file.ai\n");
   fprintf(stderr, "     -l  <file1.ai> <file.data>    learning from file.data \n");
-  fprintf(stderr, "     -cl <file.data>               creation of a network and learning from file.data\n");
-  fprintf(stderr, "     -cl <file.ai> <file.data>     creation of a default network from data in file.ai and learning from file.data\n");
   fprintf(stderr, "     -w  <file.ai> <file.data>     apply the network on the datas\n\n\n");
 }
 
